fb30b0e20b3755d9eb43f3b98504f89684e754b6
OrionSleeve <akbaroff.t@yandex.ru>
2023-09-07 16:55:41 +0700

    fix: изменили тесты аннотаций

--- src/test/java/ru/yandex/practicum/filmorate/FilmTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmTest.java
@@ -1,88 +1,149 @@
 package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.controller.FilmController;
-import ru.yandex.practicum.filmorate.exception.ValidException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.services.FilmManage;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 public class FilmTest extends FilmorateApplicationTests {
-    private static FilmController filmController;
-    private static Film film;
-    private static Film film1;
+    private static Validator validator;
 
     @BeforeAll
     public static void load() {
-        filmController = new FilmController(new FilmManage());
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
     }
 
-    @BeforeEach
-    void beforeEach() {
-        film = Film.builder()
+    @Test
+    public void fieldNameIsEmptyTest() {
+        Film film = Film.builder()
+                .name("")
+                .description("Animated musical drama film")
+                .duration(88)
+                .releaseDate(LocalDate.of(1994, 6, 19))
+                .build();
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    public void fieldNameTest() {
+        Film film = Film.builder()
                 .name("The Lion King")
                 .description("Animated musical drama film")
                 .duration(88)
                 .releaseDate(LocalDate.of(1994, 6, 19))
                 .build();
-        film1 = Film.builder()
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void noDescriptionTest() {
+        Film film1 = Film.builder()
                 .name("The Pianist")
-                .description("Epic biographical war drama film")
                 .duration(150)
                 .releaseDate(LocalDate.of(2002, 9, 5))
                 .build();
-
+        Set<ConstraintViolation<Film>> violations = validator.validate(film1);
+        assertFalse(violations.isEmpty());
     }
 
     @Test
-    void createFilmTest() {
-        assertEquals(film, filmController.addFilm(film));
-        assertNotNull(filmController.getAllFilms());
-        assertTrue(filmController.getAllFilms().contains(film));
+    public void sizeDescriptionTest() {
+        Film film = Film.builder()
+                .name("The Lion King")
+                .description("Animated musical drama film")
+                .duration(88)
+                .releaseDate(LocalDate.of(1994, 6, 19))
+                .build();
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertTrue(violations.isEmpty());
     }
 
     @Test
-    void updateFilmTest() {
-        Film oldFilm = film;
-        Film newFilm = filmController.addFilm(oldFilm);
-        newFilm.setId(film.getId());
-        newFilm.setReleaseDate(LocalDate.of(1992, 11, 8));
-        newFilm.setName("Aladdin");
-        Film update = filmController.updateFilm(newFilm);
-        assertNotNull(update, "Film not updated");
-        assertEquals(newFilm, update, "Film not equals");
+    public void overSizeDescriptionTest() {
+        Film film1 = Film.builder()
+                .name("The Pianist")
+                .description("In September 1939, Władysław Szpilman, a Polish-Jewish pianist, is playing live on the" +
+                        " radio in Warsaw when the station is bombed during Nazi Germany's invasion of Poland. Hoping" +
+                        " for a quick victory, Szpilman rejoices with his family at home when he learns that Britain" +
+                        " and France have declared war on Germany, but the promised aid does not come.\n" +
+                        "\n" +
+                        "Fighting lasts for just over a month, with both the German and Soviet armies invading Poland" +
+                        " at the same time on different fronts. Warsaw becomes part of the Nazi-controlled General" +
+                        " Government. Jews are soon prevented from working or owning businesses, and are also made to" +
+                        " wear blue Star of David armbands.\n" +
+                        "\n" +
+                        "By November 1940, Szpilman and his family are forced from their home into the isolated and" +
+                        " overcrowded Warsaw Ghetto, where conditions only get worse. People starve, the SS guards are" +
+                        " brutal, starving children are abandoned and dead bodies are everywhere. On one occasion, the" +
+                        " Szpilmans witness the SS kill an entire family in an apartment across the street during" +
+                        " a round-up, including throwing a wheelchair-bound man from a window. On another" +
+                        " occasion, Szpilman witnesses an SS guard torturing a young boy to death behind a wall while" +
+                        " walking around the ghetto.")
+                .duration(150)
+                .releaseDate(LocalDate.of(2002, 9, 5))
+                .build();
+        Set<ConstraintViolation<Film>> violations = validator.validate(film1);
+        assertFalse(violations.isEmpty());
     }
 
     @Test
-    void descriptionTest() {
-        film.setDescription("Мультфильм рассказывает историю Симбы, юного льва, который должен стать преемником своего" +
-                " отца Муфасы — короля Земель Прайда; однако после того как дядя Симбы Шрам убивает Муфасу, чтобы" +
-                " захватить трон, Симба чувствует себя виновным в смерти отца и бежит из Земель Прайда." +
-                " Повзрослев в компании своих новых друзей — суриката Тимона и бородавочника Пумбы, — Симба получает" +
-                " поддержку от своей возлюбленной Налы и от королевского шамана, мандрила Рафики и возвращается" +
-                " в Земли Прайда, чтобы бросить вызов Шраму, положить конец его тирании и занять место в Круге жизни" +
-                " в качестве законного короля.");
-        Exception exception = assertThrows(ValidException.class, () -> filmController.addFilm(film));
-        assertEquals("Maximum description length 200 characters", exception.getMessage());
+    public void releaseDateInvalidTest() {
+        Film film = Film.builder()
+                .name("The Lion King")
+                .description("Animated musical drama film")
+                .duration(88)
+                .releaseDate(LocalDate.of(1799, 6, 19))
+                .build();
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertFalse(violations.isEmpty());
     }
 
     @Test
-    void releaseDateTest() {
-        film.setReleaseDate(LocalDate.of(1799, 5, 11));
-        Exception exception = assertThrows(ValidException.class, () -> filmController.addFilm(film));
-        assertEquals("Release date cannot be earlier 1895-12-28", exception.getMessage());
+    public void releaseDateValidTest() {
+        Film film = Film.builder()
+                .name("The Lion King")
+                .description("Animated musical drama film")
+                .duration(88)
+                .releaseDate(LocalDate.of(1994, 6, 19))
+                .build();
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertTrue(violations.isEmpty());
     }
 
     @Test
-    void durationTest() {
-        film.setDuration(-15);
-        assertThrows(ValidException.class, () -> filmController.addFilm(film));
+    public void durationValidTest() {
+        Film film = Film.builder()
+                .name("The Lion King")
+                .description("Animated musical drama film")
+                .duration(88)
+                .releaseDate(LocalDate.of(1994, 6, 19))
+                .build();
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertTrue(violations.isEmpty());
     }
 
-
+    @Test
+    public void durationInValidTest() {
+        Film film = Film.builder()
+                .name("The Lion King")
+                .description("Animated musical drama film")
+                .duration(-254)
+                .releaseDate(LocalDate.of(1994, 6, 19))
+                .build();
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertFalse(violations.isEmpty());
+    }
 }
