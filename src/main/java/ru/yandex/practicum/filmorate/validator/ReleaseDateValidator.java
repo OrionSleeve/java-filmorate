1fdf6f69f2e5aaf8dff15edfeee4121510ff4867
OrionSleeve <akbaroff.t@yandex.ru>
2023-09-12 22:00:20 +0700

    new: добавлена статическая переменная DATE

--- src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidator.java
@@ -2,11 +2,14 @@ package ru.yandex.practicum.filmorate.validator;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public class ReleaseDateValidator implements ConstraintValidator<ReleaseDate, LocalDate> {
+    @NotNull
+    static final LocalDate DATE = LocalDate.of(1895, 12, 28);
     @Override
     public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
-        return localDate.isAfter(LocalDate.of(1895, 12, 28));
+        return localDate.isAfter(DATE);
     }
 }
