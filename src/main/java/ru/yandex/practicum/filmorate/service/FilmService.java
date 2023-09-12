116fb0c1c6059e0d02462aeeba1d3e1c366840fd
OrionSleeve <akbaroff.t@yandex.ru>
2023-09-12 22:02:52 +0700

    new: переработаны классы сервисы, добавлен новый функционал

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -0,0 +1,77 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class FilmService {
+    private final FilmStorage filmStorage;
+
+    @Autowired
+    public FilmService(FilmStorage filmStorage) {
+        this.filmStorage = filmStorage;
+    }
+
+    public Film addFilm(Film film) {
+        log.info("Added film with id = {} ", film.getId());
+        return filmStorage.addedFilm(film);
+    }
+
+    public Film updateFilm(Film film) {
+        log.info("Update film with id = {} ", film.getId());
+        return filmStorage.updateFilm(film);
+    }
+
+    public List<Film> getFilms() {
+        log.info("GET {} films", filmStorage.getFilms().size());
+        return filmStorage.getFilms();
+    }
+
+    public Film getFilmById(int filmId) {
+        Optional<Film> film = filmStorage.getFilmById(filmId);
+        if (film.isPresent()) {
+            log.info("get films with id = {} ", filmId);
+            return film.get();
+        } else {
+            throw new NotFoundException("Film with id = " + filmId + " not found");
+        }
+    }
+
+    public void addLikes(int filmId, int userId) {
+        Film film = getFilmById(filmId);
+        if (!film.getLikes().contains(userId)) {
+            film.getLikes().add(userId);
+            log.info("Like added to film with id = {} ", filmId);
+        } else {
+            log.debug("User not found");
+            throw new NotFoundException("User not found");
+        }
+    }
+
+    public void removeLikes(int filmId, int userId) {
+        if (filmId < 0 || userId < 0) {
+            throw new NotFoundException("negative value is not allowed");
+        }
+        Film film = getFilmById(filmId);
+        film.getLikes().remove(userId);
+        log.info("User with id = {} remove likes from the film id = {}", userId, filmId);
+    }
+
+    public List<Film> favoriteFilms(Integer number) {
+        return filmStorage.getFilms().stream()
+                .sorted(Collections.reverseOrder(Comparator.comparingInt(film -> film.getLikes().size())))
+                .limit(number)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,83 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class UserService {
+    private final UserStorage userStorage;
+
+    public UserService(UserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
+
+    public User createUser(User user) {
+        log.info("New user with id = {} created ", user.getId());
+        return userStorage.newUser(user);
+    }
+
+    public User updateUser(User user) {
+        log.info("User with id = {} {} ", user.getId(), "has been updated");
+        return userStorage.updateUser(user);
+    }
+
+    public List<User> getUsers() {
+        log.info("get {} users ", userStorage.getUsers().size());
+        return userStorage.getUsers();
+    }
+
+    public User getUserById(long id) {
+        log.info("get user with id = {} ", id);
+        Optional<User> user = userStorage.getUserById(id);
+        if (user.isPresent()) {
+            log.info("get user with id = {} ", id);
+            return user.get();
+        } else {
+            throw new NotFoundException("user with id = " + id + "not found");
+        }
+    }
+
+    public void addFriend(Long id, Long friendId) {
+        User user = getUserById(id);
+        if (user.getFriends().contains(friendId)) {
+            log.info("friend with id = {} {} {}", friendId, " has been friend of the user with id = {}", id);
+            throw new NotFoundException("User " + id + " and the user " + friendId + " have been friends yet");
+        }
+        User friend = getUserById(friendId);
+        user.getFriends().add(friendId);
+        friend.getFriends().add(id);
+
+        log.info("The friend with id = {} {} {}", friendId, " has been added to the user with id = ", id);
+        log.info("The friend with id = {} {} {}", id, " has been added to the user with id = ", friendId);
+    }
+
+    public void removeFriendById(long id, Long friendId) {
+        User user = getUserById(id);
+        log.info("friend with id = {}{}{}", friendId, " has been removed to the user with id = ", id);
+        user.getFriends().remove(friendId);
+    }
+
+    public List<User> getAllFriends(long id) {
+        log.info("get all friends");
+        return getUserById(id).getFriends()
+                .stream()
+                .map(this::getUserById)
+                .collect(Collectors.toList());
+    }
+
+    public List<User> getCommonFriends(long userId, long friendId) {
+        log.info("get common friends");
+        List<User> friends = getAllFriends(userId);
+        friends.retainAll(getAllFriends(friendId));
+        return friends;
+    }
+
+}
