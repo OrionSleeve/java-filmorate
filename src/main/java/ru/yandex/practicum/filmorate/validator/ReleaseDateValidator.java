00a9d4020180e54e6ff5997929ba839c780a4d56
OrionSleeve <akbaroff.t@yandex.ru>
2023-09-12 22:21:27 +0700

    fix: codeStyle

--- src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidator.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 public class ReleaseDateValidator implements ConstraintValidator<ReleaseDate, LocalDate> {
     @NotNull
     static final LocalDate DATE = LocalDate.of(1895, 12, 28);
+
     @Override
     public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
         return localDate.isAfter(DATE);
