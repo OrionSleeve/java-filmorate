f7c6a154f179d189fe667dfc9efcb3a4f8d0d86e
OrionSleeve <akbaroff.t@yandex.ru>
2023-09-28 04:43:45 +0700

    new: Sprint 11

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -11,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 public class GenreService {
     private final GenreStorage genreStorage;
+
     public List<Genre> getAllGenres() {
         return genreStorage.getAllGenres();
     }
